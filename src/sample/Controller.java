574e529d4e11fef70b93b79e73a8b531fda94665
vidlakparek <accounts@marek-vidlak.cz>
2020-11-22 16:59:20 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TextField mail;
+    public PasswordField pass;
+    public Button log_butt;
+
+
+    public void login(ActionEvent actionEvent) {
+
+    }
+}
