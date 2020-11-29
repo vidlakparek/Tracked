29c9e1edb38b27307f6d930b2a00258f1ea1c441
vidlakparek <accounts@marek-vidlak.cz>
2020-11-29 21:17:29 +0100

    kokotina

--- src/sample/ControllerLoged.java
+++ src/sample/ControllerLoged.java
@@ -7,31 +7,23 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ControllerLoged {
 
-    public TableColumn<?, ?> idUkol;
-    public TableColumn<?, ?> ukol;
 
+    public Pane task;
+    int color;
 
+   // @Override
+    public void initialize(){
 
-    public void change(ActionEvent event){
-
-        Parent Par = null;
-        try {
-            Par = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Scene sample = new Scene(Par,700,500);
-        Stage okno = (Stage)((Node)event.getSource()).getScene().getWindow();
-        okno.setScene(sample);
-        okno.show();
+        task.setStyle("-fx-background-color: #"+color+"");
     }
-
     public void log_out(ActionEvent event) {
         try {
             Controller.pripojeni.close();
