284331a0d9fbad487f7848285b9c0848d11ad89a
kuba65 <kubapaidar@seznam.cz>
2020-12-14 22:16:27 +0100

    Vytváření objektů6 (Done) Vytváření nových úkolů - Ošetřit vstupy Funkční refresh.

--- src/sample/ControllerLoged.java
+++ src/sample/ControllerLoged.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -9,11 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -22,23 +19,91 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 public class ControllerLoged {
     public AnchorPane arPane;
     public ScrollPane scroll;
     public Button butt[];
     public FlowPane fPane;
+    public Label timeLabel;
+    public Task ukoly[];
+
 
     public void initialize(){
-        AddTasks.addTask();
+        createUkoly();
+        addTasks();
         initializeButtons();
+
+    }
+
+
+
+    public void createUkoly(){
+        try {
+            Class.forName( "com.mysql.jdbc.Driver" );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Connection conn = Controller.getConnection();
+        try {
+            PreparedStatement dotaz = conn.prepareStatement("SELECT * FROM Tasks");
+            ResultSet vysledky = dotaz.executeQuery();
+            int i = 0;
+            while (vysledky.next()){i++;}
+            ukoly = new Task[i];
+
+        } catch (SQLException throwables) {
+        throwables.printStackTrace();
+        }
+    }
+
+    public void addTasks(){
+        ukoly[0]= new Task(0,null,null,null,0,null,null,false);
+        int DID = 0;
+        String Dname = null;
+        String Ddesc = null;
+        Date Ddeadline = null;
+        int Dpriority = 0;
+        String Ddir_users = null;
+        String Dgroups = null;
+        boolean Dstav = false;
+        try {
+            Class.forName( "com.mysql.jdbc.Driver" );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Connection conn = Controller.getConnection();
+        try {
+            PreparedStatement dotaz = conn.prepareStatement("SELECT * FROM Tasks");
+            ResultSet vysledky = dotaz.executeQuery();
+
+            int i = 0;
+            while (vysledky.next()) {
+                DID = vysledky.getInt(1);
+                Dname = vysledky.getString(2);
+                System.out.println(Dname);
+                Ddesc = vysledky.getString(3);
+                Ddeadline = vysledky.getDate(4);
+                Dpriority = vysledky.getInt(5);
+                Ddir_users = vysledky.getString(6);
+                Dgroups = vysledky.getString(7);
+                Dstav = vysledky.getBoolean(8);
+                ukoly[i] = new Task(DID,Dname,Ddesc,Ddeadline,Dpriority,Ddir_users,Dgroups,Dstav);
+                i++;
+
+            }
+            dotaz.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void initializeButtons(){
-        butt = new Button[AddTasks.getLength()];
+        butt = new Button[ukoly.length];
         butt[0] = new Button("");
-        for(int i = 0;i<AddTasks.getLength();i++) {
-            System.out.println(Task.getNameFromArray(2)+Task.getDeadlineFromArray(i));
-            butt[i] = new Button(Task.getNameFromArray(i) +"\n" +"\n" +"\n" +"\n" + Task.getDeadlineFromArray(i));
+        for(int i = 0;i< ukoly.length;i++) {
+            System.out.println(ukoly[i].getName()+ukoly[i].getDeadlline());
+            butt[i] = new Button(ukoly[i].getName()+ "\n"+"\n"+"\n"+"\n"+ukoly[i].getDeadlline());
             butt[i].setLayoutX(10);
             butt[i].setLayoutY(40 + i*100);
             butt[i].setId("button"+i);
@@ -79,6 +144,9 @@ public class ControllerLoged {
     }
 
     public void refresh(ActionEvent event) {
+        createUkoly();
+        addTasks();
+        initializeButtons();
         /*Aktualizace seznamu tasks, stejně jako při stisknutí klávesy F5*/
     }
 
--- src/sample/Task.java
+++ src/sample/Task.java
@@ -1,92 +1,54 @@
 package sample;
 
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Date;
 
 public class Task {
-    static int ID;
-    static String name;
-    static String desc;
-    static Date deadline;
-    static int priority;
-    static String dir_users;
-    static int groups;
-    static boolean stav;
+    private int ID;
+    private String name;
+    private String desc;
+    private Date deadline;
+    private int priority;
+    private String dir_users;
+    private String groups;
+    private boolean stav;
 
 
 
-    public Task(int ID, String name, String desc, Date deadline, int priority, String dir_users, int groups, boolean stav){
-    Task.ID = ID;
-    Task.name = name;
-    Task.desc = desc;
-    Task.deadline = deadline;
-    Task.priority = priority;
-    Task.dir_users = dir_users;
-    Task.groups = groups;
-    Task.stav = stav;
+
+    public Task(int ID, String name, String desc, Date deadline, int priority, String dir_users, String groups, boolean stav){
+    this.ID = ID;
+    this.name = name;
+    this.desc = desc;
+    this.deadline = deadline;
+    this.priority = priority;
+    this.dir_users = dir_users;
+    this.groups = groups;
+    this.stav = stav;
     }
-
-    public static String getName(Object a){
+    public int getID(){
+        return ID;
+    }
+    public String getName(){
         return name;
     }
-
-    public static String getDesc(Object a){
+    public String getDesc(){
         return desc;
     }
-
-    public static Date getDeadLine(Object a){
+    public Date getDeadlline(){
         return deadline;
     }
-
-    public static int getPriority(Object a){
+    public int getPriority(){
         return priority;
     }
-
-    public static String getDir_users(Object a){return dir_users;}
-
-    public static String getAll(Task a){return name + desc;}
-
-    public static int getGroups(Object a){
+    public String getDir_users(){
+        return dir_users;
+    }
+    public String getGroups(){
         return groups;
     }
-
-    public static boolean getStav(Object a){
+    public boolean getStav(){
         return stav;
     }
-
-    public static String getNameFromArray(int index){
-        return getName(AddTasks.tasks.get(index));
-    }
-
-    public static String getDescFromArray(int index){
-        return getDesc(AddTasks.tasks.get(index));
-    }
-
-    public static Date getDeadlineFromArray(int index){
-        return getDeadLine(AddTasks.tasks.get(index));
-    }
-
-    public static int getPriorityFromArray(int index){
-        return getPriority(AddTasks.tasks.get(index));
-    }
-
-    public static String getDir_usersFromArray(int index){
-        return getDir_users(AddTasks.tasks.get(index));
-    }
-
-    public static int getGroupsFromArray(int index){
-        return getGroups(AddTasks.tasks.get(index));
-    }
-
-    public static boolean getStavFromArray(int index){
-        return getStav(AddTasks.tasks.get(index));
-    }
-
-    public static String getAllfromArray(int index){return getAll(AddTasks.tasks.get(index));}
 }
