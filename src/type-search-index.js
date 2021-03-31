8d805af7a51d4d35c507d4bcf2f03b533e9fe391
vidlakparek <accounts@marek-vidlak.cz>
2021-03-31 11:19:47 +0200

    JavaDoc generated

---
+++ src/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"sample","l":"Controller"},{"p":"sample","l":"ControllerLoged"},{"p":"sample","l":"CreateTask"},{"p":"sample","l":"Main"},{"p":"sample","l":"ControllerLoged.nameComparator"},{"p":"sample","l":"ShowTask"},{"p":"sample","l":"Task"}];updateSearchResults();
